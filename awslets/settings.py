c352359860ab3315c1b0ebfc828195166e032019
rajmohanperiyasamy <rajmohanjr@gmail.com>
2024-12-24 23:35:28 +0530

    Database settings

--- awslets/settings.py
+++ awslets/settings.py
@@ -74,10 +74,19 @@ WSGI_APPLICATION = 'awslets.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': BASE_DIR / 'ilets.cnf',
+        },
     }
 }
 
