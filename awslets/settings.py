5d618516f678fc4788ec1d3f14ee20d8b1440d37
rajmohanperiyasamy <rajmohanjr@gmail.com>
2024-12-27 00:34:23 +0530

    letsin

--- awslets/settings.py
+++ awslets/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t3n*f7h&imf%+cfmeg@l_lk=n$k7re+mlje&=g$_g%!84^7ic8
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','51.20.135.189','speed.cloudflare.com','http://www.letsfix.in/',
-                 'ec2-51-20-135-189.eu-north-1.compute.amazonaws.com']
+                 'ec2-51-20-135-189.eu-north-1.compute.amazonaws.com','letsfix.in']
 
 
 # Application definition
